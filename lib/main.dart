d89545aa5bfbd22cb1862ff20f2bb2128bd9fc70
Dorreen Rostami <dor.rostami@gmail.com>
2024-09-19 01:06:51 -0400

    initial

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(84, 30, 30, 176),
+          title: const Text('HomeGuard'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: const [
+          BottomNavigationBarItem(label: 'Login', icon: Icon(Icons.login)),
+          BottomNavigationBarItem(
+              label: 'Signup', icon: Icon(Icons.app_registration))
+        ]),
+      ),
+    );
+  }
+}
